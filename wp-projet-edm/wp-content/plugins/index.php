d75476025b63cdb9d7834ab78ad78ddd94d23613
ShikamaruNara21 <85990379+ShikamaruNara21@users.noreply.github.com>
2021-09-24 12:23:03 +0200

    Update index.php

--- wp-projet-edm/wp-content/plugins/index.php
+++ wp-projet-edm/wp-content/plugins/index.php
@@ -1,2 +1,29 @@
 <?php
-// Silence is golden.
+/*
+  Plugin Name: mes shortcodes
+  Description: Plugin fournissant des shortcodes
+  Author: Elliott
+  Version: 1.0.0
+ */
+
+function shortcode_bienvenue(){
+    return "<h2>Bienvenue chez karac !</h2>";
+}
+add_shortcode('bienvenue', 'shortcode_bienvenue');
+
+
+function shortcode_agence($atts){
+    extract(shortcode_atts(
+        array(
+    	    'langue' => 'EN'
+    ), $atts));
+
+    if($langue== "EN"){
+        $text = "karac, your digital communication agency ";
+    }
+    else{
+    	$text = "karac, votre agence de communication digitale";
+    }
+    return '<h2>' . $text . '</h2>';
+}
+add_shortcode('agence', 'shortcode_agence');
